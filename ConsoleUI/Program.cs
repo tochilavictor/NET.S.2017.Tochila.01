ff91b69e270a9a9603a8c3a90b98d3dc1d6059b9
tochilavictor <tochilavictor@gmail.com>
2017-06-27 15:18:37 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortLogic;
+using System.Diagnostics;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            int[] mas = new int[10000];
+            int[] mas2 = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                mas[i] = rand.Next(1000);
+                mas2[i] = rand.Next(1000);
+            }
+            stopWatch.Start();
+            QuickSort.Sort(mas);
+            stopWatch.Stop();
+            TimeSpan quicksorttime = stopWatch.Elapsed;
+            stopWatch.Restart();
+            stopWatch.Start();
+            MergeSort.Sort(mas2);
+            stopWatch.Stop();
+            TimeSpan mergesorttime = stopWatch.Elapsed;
+            Console.WriteLine("Quick sort time " + quicksorttime.TotalMilliseconds +" milliseconds");
+            Console.WriteLine("Merge sort time " + mergesorttime.TotalMilliseconds +" milliseconds");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortLogic/QuickSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortLogic
+{
+    /// <summary>
+    ///  implements the quicksort algorithm for integers
+    /// </summary>
+    public static class QuickSort
+    {
+        /// <summary>
+        /// Implements the quick sort algorithm for integers array
+        /// </summary>
+        /// <param name="ar">array</param>
+        public static void Sort(int[] ar)
+        {
+            Sort(ar, 0, ar.Length - 1);
+        }
+        static void Sort(int[] a, int left, int right)
+        {
+            int x = a[(left + right) / 2];
+            int i = left;
+            int j = right;
+            while (i <= j)
+            {
+                while (a[i] < x) i++;
+                while (a[j] > x) j--;
+                if (i <= j)
+                {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < right) Sort(a, i, right);
+            if (left < j) Sort(a, left, j);
+        }
+    }
+}
